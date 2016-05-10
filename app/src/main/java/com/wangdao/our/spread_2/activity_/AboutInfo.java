476aa62fd78d007f29d4bfb79d34228bff29bc62
unknown <632985226@qq.com>
2016-05-10 15:55:20 +0800

    第二次提交-创建用户20160.10

--- app/src/main/java/com/wangdao/our/spread_2/activity_/AboutInfo.java
+++ app/src/main/java/com/wangdao/our/spread_2/activity_/AboutInfo.java
@@ -342,6 +342,7 @@ new Thread(new Runnable() {
     helpdialog_item_2 hi_2 = null;
     private void showPhotoDialog() {
         if(hi_2==null){
+
             hi_2= new helpdialog_item_2();
             view_2 = getLayoutInflater().inflate(R.layout.dialog_out_login, null);
             hi_2.tv_help1 = (TextView) view_2.findViewById(R.id.bt_help1);
